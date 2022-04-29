d1b6c38d754cd8df3bfabde7769e6a1a539eac62
haribabuperla04 <93529821+HaribabuPerla1999@users.noreply.github.com>
2022-04-29 12:00:09 +0530

    fresh project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Todo from "./components/Todo.jsx";
+
 
 
 function App() {
   return (
     <div>
       <h2>TodoItems</h2>
-      <Todo text="Learn React"/>
-      <Todo text = "Learn Js"/>
-      <Todo text = "Learn NextJS"/>
     
      
     </div>
